4a078510ddb04f6fd94dec189f446bc1de646165
Peter Nicholson <peternicholson31@gmail.com>
2025-11-08 10:18:38 +0100

    Add healthz endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from friend-finder!")
+from flask import Flask, jsonify
+import os
 
+app = Flask(__name__)
+
+@app.route("/healthz", methods=["GET"])
+def healthz():
+    return jsonify(status="ok"), 200
 
 if __name__ == "__main__":
-    main()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=5000)
