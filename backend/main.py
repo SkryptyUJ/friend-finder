ce34a5b23cbc98b48eb4926bc0fce743a690a30f
Peter Nicholson <peternicholson31@gmail.com>
2025-12-06 17:53:51 +0100

    Hard-code Redis URL

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*", ping_timeout=60)
+socketio = SocketIO(app, cors_allowed_origins="*", ping_timeout=60, message_queue="redis://red-d4q5ujruibrs73dg30gg:6379")
 
 locations = {}
 
